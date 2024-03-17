c763464e1016805e84668d197d6cc376ec42df89
Tamás Cserteg <csertegt@gmail.com>
2024-03-17 17:56:31 +0100

    frunctions enable frustum sampling

--- src/primitives/frustum.jl
+++ src/primitives/frustum.jl
@@ -35,6 +35,37 @@ height(f::Frustum) = height(boundary(f))
 
 axis(f::Frustum) = axis(boundary(f))
 
+paramdim(::Type{<:Frustum}) = 3
+
+axis(f::Frustum) = Line(center(bottom(f)), center(top(f)))
+
+# around the axis, along the radius, height
+function (f::Frustum{T})(φ, r, z) where {T}
+  if (φ < 0 || φ > 1) || (z < 0 || z > 1) || (r < 0 || r > 1)
+    throw(DomainError((φ, r, z), "f(φ, r, z) is not defined for φ, r, z outside [0, 1]³."))
+  end
+  rb = radius(bottom(f))
+  rt = radius(top(f))
+  a = axis(f)
+  d = a(1) - a(0)
+  l = norm(d)
+
+  # rotation to align z axis with cylinder axis
+  Q = rotation_between(d, Vec{3,T}(0, 0, 1))
+
+  # scale coordinates
+  φₛ = 2T(π) * φ
+  zₛ = z * l
+  rₛ = r * (rb * (l - zₛ) + rt * zₛ)
+
+  x_local = cos(φₛ) * rₛ / l
+  y_local = sin(φₛ) * rₛ / l
+  z_local = zₛ
+  p_local = Vec{3,T}(x_local, y_local, z_local)
+
+  center(bottom(f)) + Q' * p_local
+end
+
 function Random.rand(rng::Random.AbstractRNG, ::Random.SamplerType{Frustum{T}}) where {T}
   bottom = rand(rng, Disk{T})
   ax = normal(plane(bottom))
